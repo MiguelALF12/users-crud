66b0d44f201497f4446e737f610f98918ddb4efc
MiguelALF12 <flepard3612@gmail.com>
2025-01-25 22:13:25 -0500

    Add user CRUD routes and login functionality

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('title', 'Login')
+
+@section('content')
+<div class="container mx-auto p-4">
+    <h1 class="text-2xl font-bold mb-4">Login</h1>
+    <form action="{{ route('login') }}" method="POST">
+        @csrf
+        <div class="mb-4">
+            <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+            <input type="email" id="email" name="email" class="mt-1 block w-full border-gray-300 rounded-md" required>
+        </div>
+        <div class="mb-4">
+            <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
+            <input type="password" id="password" name="password" class="mt-1 block w-full border-gray-300 rounded-md" required>
+        </div>
+        <div class="mb-4">
+            <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded">Login</button>
+        </div>
+        @if ($errors->any())
+        <div class="text-red-500">
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
+    </form>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,22 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\Api\User\UserListController;
+use App\Http\Controllers\Api\User\UserCreateController;
+use App\Http\Controllers\Api\User\UserEditController;
+use App\Http\Controllers\Api\User\UserDeleteController;
+use App\Http\Controllers\Auth\LoginController;
 
-Route::get('/', [UserController::class, 'index'])->name('users.index');
-Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-Route::post('/users', [UserController::class, 'store'])->name('users.store');
-Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
-Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
-Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+//Auth routes
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+
+// User routes
+Route::get('/users', [UserListController::class, 'index'])->name('users.index');
+Route::get('/users/create', [UserCreateController::class, 'create'])->name('users.create');
+Route::post('/users', [UserCreateController::class, 'store'])->name('users.store');
+Route::get('/users/{userId}/edit', [UserEditController::class, 'edit'])->name('users.edit');
+Route::put('/users/{userId}', [UserEditController::class, 'update'])->name('users.update');
+Route::delete('/users/{user}', [UserDeleteController::class, 'destroy'])->name('users.destroy');
