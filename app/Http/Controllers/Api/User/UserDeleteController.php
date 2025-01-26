c92d106d22702a8690c6ef84d81cd628b52c20f1
MiguelALF12 <flepard3612@gmail.com>
2025-01-26 17:06:00 -0500

    Modify delete user controller, adding exception handling

--- app/Http/Controllers/Api/User/UserDeleteController.php
+++ app/Http/Controllers/Api/User/UserDeleteController.php
@@ -3,15 +3,64 @@
 namespace App\Http\Controllers\Api\User;
 
 use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use App\Core\Users\ManagerUsers;
+use Illuminate\Support\Facades\DB;
+
 
 class UserDeleteController
 {
+    protected $mu;
 
-    public function destroy($id)
+    public function __construct()
     {
-        // Logic to delete a user
-        $user = User::find($id);
-        $user->delete();
-        return redirect()->route('users.index');
+        $this->mu = new ManagerUsers();
+    }
+    public function destroy(Request $request, $id)
+    {
+        try {
+            if (is_null($id) && !is_numeric($id)) {
+                session()->flash('error', 'Invalid user ID.');
+                return back();
+            }
+            $params = [
+                'id' => $id
+            ];
+        } catch (\Exception $e) {
+            Log::info($e->getMessage());
+            session()->flash('error', 'Validation failed. Please check the form and try again.');
+            return redirect()->route('users.index');
+        }
+
+        try {
+            return DB::transaction(function () use ($request, $params) {
+                $objRemoved = $this->deleteUser($params);
+                if ($objRemoved) {
+                    session()->flash('notify', [
+                        'type' => 'success',
+                        'message' => 'The user was deleted successfully.'
+                    ]);
+                } else {
+                    session()->flash('notify', [
+                        'type' => 'error',
+                        'message' => 'Failed to delete the user. Please try again.'
+                    ]);
+                }
+                return redirect()->route('users.index');
+            });
+        } catch (\Exception $e) {
+            Log::info($e->getMessage());
+            session()->flash('notify', [
+                'type' => 'error',
+                'message' => 'Failed to delete the user. Please try again.'
+            ]);
+            return redirect()->route('users.index');
+        }
+    }
+
+    public function deleteUser($params)
+    {
+        return $this->mu->deleteUser($params['id']);
     }
 }
